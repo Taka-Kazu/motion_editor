9d39d2772fdbf05f03386874979dd66e2b78c2d3
taka-kazu <tkhsh.schoen@gmail.com>
2017-11-23 20:06:50 +0900

    ボタンを並べた

--- AHO_for_windows/AHO_for_windows/Form1.Designer.cs
+++ AHO_for_windows/AHO_for_windows/Form1.Designer.cs
@@ -30,11 +30,39 @@
         {
             this.components = new System.ComponentModel.Container();
             this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
-            this.COMComboBox = new System.Windows.Forms.ComboBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.ファイルToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.開くToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.button15 = new System.Windows.Forms.Button();
+            this.button16 = new System.Windows.Forms.Button();
+            this.button17 = new System.Windows.Forms.Button();
+            this.button18 = new System.Windows.Forms.Button();
+            this.button19 = new System.Windows.Forms.Button();
+            this.button20 = new System.Windows.Forms.Button();
+            this.button21 = new System.Windows.Forms.Button();
+            this.button22 = new System.Windows.Forms.Button();
+            this.新規ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.menuStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // serialPort1
@@ -42,15 +70,16 @@
             this.serialPort1.BaudRate = 115200;
             this.serialPort1.Parity = System.IO.Ports.Parity.Even;
             // 
-            // COMComboBox
+            // comboBox1
             // 
-            this.COMComboBox.FormattingEnabled = true;
-            this.COMComboBox.Location = new System.Drawing.Point(12, 27);
-            this.COMComboBox.Name = "COMComboBox";
-            this.COMComboBox.Size = new System.Drawing.Size(121, 20);
-            this.COMComboBox.TabIndex = 0;
-            this.COMComboBox.DropDown += new System.EventHandler(this.COMComboBox_DropDown);
-            this.COMComboBox.SelectedIndexChanged += new System.EventHandler(this.COMComboBox_SelectedIndexChanged);
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 27);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(91, 20);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.Text = "COM1";
+            this.comboBox1.DropDown += new System.EventHandler(this.COMComboBox_DropDown);
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.COMComboBox_SelectedIndexChanged);
             // 
             // menuStrip1
             // 
@@ -65,6 +94,7 @@
             // ファイルToolStripMenuItem
             // 
             this.ファイルToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.新規ToolStripMenuItem,
             this.開くToolStripMenuItem});
             this.ファイルToolStripMenuItem.Name = "ファイルToolStripMenuItem";
             this.ファイルToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
@@ -76,19 +106,289 @@
             this.開くToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
             this.開くToolStripMenuItem.Text = "開く";
             // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(12, 53);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(91, 20);
+            this.comboBox2.TabIndex = 2;
+            this.comboBox2.Text = "115200";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 79);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(91, 39);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "接続";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 383);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.ForcedVertical;
+            this.richTextBox1.Size = new System.Drawing.Size(680, 106);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button18);
+            this.groupBox1.Controls.Add(this.button19);
+            this.groupBox1.Controls.Add(this.button20);
+            this.groupBox1.Controls.Add(this.button21);
+            this.groupBox1.Controls.Add(this.button22);
+            this.groupBox1.Controls.Add(this.button13);
+            this.groupBox1.Controls.Add(this.button14);
+            this.groupBox1.Controls.Add(this.button15);
+            this.groupBox1.Controls.Add(this.button16);
+            this.groupBox1.Controls.Add(this.button17);
+            this.groupBox1.Controls.Add(this.button8);
+            this.groupBox1.Controls.Add(this.button9);
+            this.groupBox1.Controls.Add(this.button10);
+            this.groupBox1.Controls.Add(this.button11);
+            this.groupBox1.Controls.Add(this.button12);
+            this.groupBox1.Controls.Add(this.button7);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.button6);
+            this.groupBox1.Controls.Add(this.button5);
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Location = new System.Drawing.Point(109, 27);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(583, 350);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "モーション";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(133, 89);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(99, 46);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "POS1";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(238, 89);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(99, 46);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "POS2";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(343, 89);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(99, 46);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "POS3";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(448, 89);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(99, 46);
+            this.button5.TabIndex = 3;
+            this.button5.Text = "POS4";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(28, 141);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(99, 46);
+            this.button6.TabIndex = 4;
+            this.button6.Text = "POS5";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 18);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 19);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.Text = "hoge.csv";
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(112, 16);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "保存";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(28, 193);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(99, 46);
+            this.button8.TabIndex = 11;
+            this.button8.Text = "POS10";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(448, 141);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(99, 46);
+            this.button9.TabIndex = 10;
+            this.button9.Text = "POS9";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(343, 141);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(99, 46);
+            this.button10.TabIndex = 9;
+            this.button10.Text = "POS8";
+            this.button10.UseVisualStyleBackColor = true;
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(238, 141);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(99, 46);
+            this.button11.TabIndex = 8;
+            this.button11.Text = "POS7";
+            this.button11.UseVisualStyleBackColor = true;
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(133, 141);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(99, 46);
+            this.button12.TabIndex = 7;
+            this.button12.Text = "POS6";
+            this.button12.UseVisualStyleBackColor = true;
+            // 
+            // button13
+            // 
+            this.button13.Location = new System.Drawing.Point(28, 245);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(99, 46);
+            this.button13.TabIndex = 16;
+            this.button13.Text = "POS15";
+            this.button13.UseVisualStyleBackColor = true;
+            // 
+            // button14
+            // 
+            this.button14.Location = new System.Drawing.Point(448, 193);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(99, 46);
+            this.button14.TabIndex = 15;
+            this.button14.Text = "POS14";
+            this.button14.UseVisualStyleBackColor = true;
+            // 
+            // button15
+            // 
+            this.button15.Location = new System.Drawing.Point(343, 193);
+            this.button15.Name = "button15";
+            this.button15.Size = new System.Drawing.Size(99, 46);
+            this.button15.TabIndex = 14;
+            this.button15.Text = "POS13";
+            this.button15.UseVisualStyleBackColor = true;
+            // 
+            // button16
+            // 
+            this.button16.Location = new System.Drawing.Point(238, 193);
+            this.button16.Name = "button16";
+            this.button16.Size = new System.Drawing.Size(99, 46);
+            this.button16.TabIndex = 13;
+            this.button16.Text = "POS12";
+            this.button16.UseVisualStyleBackColor = true;
+            // 
+            // button17
+            // 
+            this.button17.Location = new System.Drawing.Point(133, 193);
+            this.button17.Name = "button17";
+            this.button17.Size = new System.Drawing.Size(99, 46);
+            this.button17.TabIndex = 12;
+            this.button17.Text = "POS11";
+            this.button17.UseVisualStyleBackColor = true;
+            // 
+            // button18
+            // 
+            this.button18.Location = new System.Drawing.Point(28, 89);
+            this.button18.Name = "button18";
+            this.button18.Size = new System.Drawing.Size(99, 46);
+            this.button18.TabIndex = 21;
+            this.button18.Text = "POS0";
+            this.button18.UseVisualStyleBackColor = true;
+            // 
+            // button19
+            // 
+            this.button19.Location = new System.Drawing.Point(448, 245);
+            this.button19.Name = "button19";
+            this.button19.Size = new System.Drawing.Size(99, 46);
+            this.button19.TabIndex = 20;
+            this.button19.Text = "POS19";
+            this.button19.UseVisualStyleBackColor = true;
+            // 
+            // button20
+            // 
+            this.button20.Location = new System.Drawing.Point(343, 245);
+            this.button20.Name = "button20";
+            this.button20.Size = new System.Drawing.Size(99, 46);
+            this.button20.TabIndex = 19;
+            this.button20.Text = "POS18";
+            this.button20.UseVisualStyleBackColor = true;
+            // 
+            // button21
+            // 
+            this.button21.Location = new System.Drawing.Point(238, 245);
+            this.button21.Name = "button21";
+            this.button21.Size = new System.Drawing.Size(99, 46);
+            this.button21.TabIndex = 18;
+            this.button21.Text = "POS17";
+            this.button21.UseVisualStyleBackColor = true;
+            // 
+            // button22
+            // 
+            this.button22.Location = new System.Drawing.Point(133, 245);
+            this.button22.Name = "button22";
+            this.button22.Size = new System.Drawing.Size(99, 46);
+            this.button22.TabIndex = 17;
+            this.button22.Text = "POS16";
+            this.button22.UseVisualStyleBackColor = true;
+            // 
+            // 新規ToolStripMenuItem
+            // 
+            this.新規ToolStripMenuItem.Name = "新規ToolStripMenuItem";
+            this.新規ToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.新規ToolStripMenuItem.Text = "新規";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
             this.ClientSize = new System.Drawing.Size(704, 501);
-            this.Controls.Add(this.COMComboBox);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "AHO for windows";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
             this.Load += new System.EventHandler(this.Form1_Load);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -97,10 +397,37 @@
         #endregion
 
         private System.IO.Ports.SerialPort serialPort1;
-        private System.Windows.Forms.ComboBox COMComboBox;
+        private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem ファイルToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem 開くToolStripMenuItem;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button18;
+        private System.Windows.Forms.Button button19;
+        private System.Windows.Forms.Button button20;
+        private System.Windows.Forms.Button button21;
+        private System.Windows.Forms.Button button22;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Button button15;
+        private System.Windows.Forms.Button button16;
+        private System.Windows.Forms.Button button17;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.ToolStripMenuItem 新規ToolStripMenuItem;
     }
 }
 
--- AHO_for_windows/AHO_for_windows/Form1.cs
+++ AHO_for_windows/AHO_for_windows/Form1.cs
@@ -31,12 +31,49 @@ namespace AHO_for_windows
 
         private void COMComboBox_DropDown(object sender, EventArgs e)
         {
-            //MessageBox.Show("彡(゜)(゜)");
+            comboBox1.Items.Clear();
             ports = SerialPort.GetPortNames();
             foreach (var item in ports)
             {
-                COMComboBox.Items.Add(item);
+                comboBox1.Items.Add(item);
             }
+            
+        }
+
+        private void button1_Click(object sender, EventArgs ex)
+        {
+            try
+            {
+                serialPort1.PortName = comboBox1.Text;
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show("(´･ω･`)利用可能なCOMポートが選ばれていないよ");
+                return;
+            }
+            try
+            {
+                serialPort1.BaudRate = int.Parse(comboBox2.Text);
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show("(´･ω･`)無効なBaudRateだよ");
+                return;
+            }
+            try
+            {
+                serialPort1.Open();
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show("(´･ω･`)%sが開けなかったよ", serialPort1.PortName);
+                return;
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            serialPort1.Close();
         }
     }
 }
