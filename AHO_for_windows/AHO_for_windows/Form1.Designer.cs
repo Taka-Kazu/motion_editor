ebe74f531907ddff116a9393a8ed77670891a469
taka-kazu <tkhsh.schoen@gmail.com>
2017-11-23 19:26:04 +0900

    コンボボックスの処理を書いた

--- AHO_for_windows/AHO_for_windows/Form1.Designer.cs
+++ AHO_for_windows/AHO_for_windows/Form1.Designer.cs
@@ -30,6 +30,11 @@
         {
             this.components = new System.ComponentModel.Container();
             this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.COMComboBox = new System.Windows.Forms.ComboBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.ファイルToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.開くToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // serialPort1
@@ -37,20 +42,65 @@
             this.serialPort1.BaudRate = 115200;
             this.serialPort1.Parity = System.IO.Ports.Parity.Even;
             // 
+            // COMComboBox
+            // 
+            this.COMComboBox.FormattingEnabled = true;
+            this.COMComboBox.Location = new System.Drawing.Point(12, 27);
+            this.COMComboBox.Name = "COMComboBox";
+            this.COMComboBox.Size = new System.Drawing.Size(121, 20);
+            this.COMComboBox.TabIndex = 0;
+            this.COMComboBox.DropDown += new System.EventHandler(this.COMComboBox_DropDown);
+            this.COMComboBox.SelectedIndexChanged += new System.EventHandler(this.COMComboBox_SelectedIndexChanged);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ファイルToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(704, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // ファイルToolStripMenuItem
+            // 
+            this.ファイルToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.開くToolStripMenuItem});
+            this.ファイルToolStripMenuItem.Name = "ファイルToolStripMenuItem";
+            this.ファイルToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.ファイルToolStripMenuItem.Text = "ファイル";
+            // 
+            // 開くToolStripMenuItem
+            // 
+            this.開くToolStripMenuItem.Name = "開くToolStripMenuItem";
+            this.開くToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.開くToolStripMenuItem.Text = "開く";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(704, 501);
+            this.Controls.Add(this.COMComboBox);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "AHO for windows";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.ComboBox COMComboBox;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem ファイルToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 開くToolStripMenuItem;
     }
 }
 
--- AHO_for_windows/AHO_for_windows/Form1.cs
+++ AHO_for_windows/AHO_for_windows/Form1.cs
@@ -7,14 +7,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO.Ports;
 
 namespace AHO_for_windows
 {
     public partial class Form1 : Form
     {
+        string[] ports;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void COMComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void COMComboBox_DropDown(object sender, EventArgs e)
+        {
+            //MessageBox.Show("彡(゜)(゜)");
+            ports = SerialPort.GetPortNames();
+            foreach (var item in ports)
+            {
+                COMComboBox.Items.Add(item);
+            }
         }
     }
 }
